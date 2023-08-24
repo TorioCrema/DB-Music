3385e198454b306b85ff4446d0650f885c528a35
Denis Caushaj <91290254+deniscaushaj@users.noreply.github.com>
2023-08-24 19:40:39 +0200

    Refactor class name

---
+++ WpfDBMusicLabel/WpfDBMusicLabel/ViewModel/VModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfDBMusicLabel.Model;
+
+namespace WpfDBMusicLabel.ViewModel
+{
+    partial class VModel : ObservableRecipient
+    {
+        [ObservableProperty]
+        private List<string> actionsList = new()
+        {
+            "Tour",
+            "Album",
+            "Firmatario",
+            "Traccia",
+            "Merchandising",
+            "Prodotto",
+            "Progetto Musicale"
+        };
+    }
+}
