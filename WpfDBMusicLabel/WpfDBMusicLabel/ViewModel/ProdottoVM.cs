7ef506a873603b687f5ec7a39551d260700d1569
TorioCrema <zamasimone5@gmail.com>
2023-09-29 10:04:43 +0200

    Add bindings to ProdInsertGrid

--- WpfDBMusicLabel/WpfDBMusicLabel/ViewModel/ProdottoVM.cs
+++ WpfDBMusicLabel/WpfDBMusicLabel/ViewModel/ProdottoVM.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using WpfDBMusicLabel.musiclabeldb;
 using System.Windows;
 using System.Data.Entity;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace WpfDBMusicLabel.ViewModel
 {
@@ -20,8 +22,38 @@ namespace WpfDBMusicLabel.ViewModel
         private Prodotto? currentSelectedProduct = null;
 
         [ObservableProperty]
-        private ObservableCollection<Album> albums;
+        private Prodotto newProd = new();
 
+        [ObservableProperty]
+        private ObservableCollection<Album>? albums;
+
+        [ObservableProperty]
+        private Album? selectedAlbum = null;
+
+        [ObservableProperty]
+        private Visibility formatoVis = Visibility.Collapsed;
+
+        [ObservableProperty]
+        private bool cd = false;
+
+        [ObservableProperty]
+        private bool vinyl = false;
+
+        [ObservableProperty]
+        private bool cassette = false;
+
+        [ObservableProperty]
+        private bool giri33 = false;
+
+        [ObservableProperty]
+        private bool giri45 = false;
+
+        [ObservableProperty]
+        private string? sellPrice = null;
+
+        [ObservableProperty]
+        private string? qty = null;
+ 
         [ObservableProperty]
         private List<string> subActionsList = new()
         {
@@ -48,7 +80,19 @@ namespace WpfDBMusicLabel.ViewModel
 
         protected override void ResetInsert()
         {
-            throw new NotImplementedException();
+            _dbContext.Albums.Load();
+            Albums = _dbContext.Albums.Local.ToObservableCollection();
+        }
+
+        protected override void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+            switch (e.PropertyName)
+            {
+                case nameof(Vinyl):
+                    FormatoVis = Vinyl == true ? Visibility.Visible : Visibility.Collapsed;
+                    break;
+            }
         }
     }
 }
