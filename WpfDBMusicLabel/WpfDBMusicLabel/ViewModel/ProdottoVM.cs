9ead58a5bab7c5c23b0745da14cec5467dc47621
TorioCrema <zamasimone5@gmail.com>
2023-10-23 17:17:15 +0200

    Implement product insert

--- WpfDBMusicLabel/WpfDBMusicLabel/ViewModel/ProdottoVM.cs
+++ WpfDBMusicLabel/WpfDBMusicLabel/ViewModel/ProdottoVM.cs
@@ -22,13 +22,28 @@ namespace WpfDBMusicLabel.ViewModel
         private Prodotto? currentSelectedProduct = null;
 
         [ObservableProperty]
-        private Prodotto newProd = new();
+        private Prodotto newProd = new()
+        {
+            Nome = "",
+            Prezzo = 0,
+            QtaProdotta = 0,
+            CostoFornitura = 0,
+            DataUscita = System.DateTime.Now,
+            Descrizione = "",
+            Formato = null
+        };
 
         [ObservableProperty]
         private ObservableCollection<Album>? albums;
 
         [ObservableProperty]
+        private ObservableCollection<Produttore>? producers;
+
+        [ObservableProperty]
         private Album? selectedAlbum = null;
+
+        [ObservableProperty]
+        private Produttore? selectedProducer = null;
 
         [ObservableProperty]
         private Visibility formatoVis = Visibility.Collapsed;
@@ -47,12 +62,6 @@ namespace WpfDBMusicLabel.ViewModel
 
         [ObservableProperty]
         private bool giri45 = false;
-
-        [ObservableProperty]
-        private string? sellPrice = null;
-
-        [ObservableProperty]
-        private string? qty = null;
  
         [ObservableProperty]
         private List<string> subActionsList = new()
@@ -65,23 +74,67 @@ namespace WpfDBMusicLabel.ViewModel
             _dbContext = new();
             _dbContext.Prodotti.Load();
             Prodotti = _dbContext.Prodotti.Local.ToObservableCollection();
+            _dbContext.Produttori.Load();
+            Producers = _dbContext.Produttori.Local.ToObservableCollection();
+            _dbContext.Albums.Load();
+            Albums = _dbContext.Albums.Local.ToObservableCollection();
         }
 
         override public bool ExecuteSubAction()
         {
-            if (CurrentSelectedProduct != null)
+            switch(CurrentSubAction)
             {
-                _dbContext.Albums.Where(x => x.Prodotti.Any(y => y.Formato == CurrentSelectedProduct.Formato)).Load(); 
-                Albums = _dbContext.Albums.Local.ToObservableCollection();
-                return true;
+                case "Vedi album":
+                    if (CurrentSelectedProduct != null)
+                    {
+                        _dbContext.Albums.Load();
+                        Albums = _dbContext.Albums.Local.ToObservableCollection();
+                        return true;
+                    }
+                    return false;
+                case "Inserisci":
+                    if (InsertCheck())
+                    {
+                        NewProd.IdAlbumNavigation = SelectedAlbum;
+                        NewProd.IdProduttoreNavigation = SelectedProducer;
+                        NewProd.Tipo = Cd ? "CD" : Vinyl ? "Vinile" : "Cassetta";
+                        if (NewProd.Tipo == "Vinile")
+                        {
+                            NewProd.Formato = Giri33 ? (byte)33 : (byte)45;
+                        }
+                        _dbContext.Prodotti.Local.Add(NewProd);
+                        SaveChanges();
+                        ResetInsert();
+                        return true;
+                    }
+                    Error = "InsertCheck failed";
+                    return false;
             }
             return false;
         }
 
         protected override void ResetInsert()
         {
+            SelectedAlbum = null;
+            SelectedProducer = null;
+            NewProd = new()
+            {
+                Nome = "",
+                Prezzo = 0,
+                QtaProdotta = 0,
+                CostoFornitura = 0,
+                DataUscita = System.DateTime.Now,
+                Descrizione = "",
+                Formato = null
+            };
+            Cd = false;
+            Vinyl = false;
+            Cassette = false;
+            Giri33 = false;
+            Giri45 = false;
             _dbContext.Albums.Load();
             Albums = _dbContext.Albums.Local.ToObservableCollection();
+            CurrentSubAction = null;
         }
 
         protected override void OnPropertyChanged(PropertyChangedEventArgs e)
@@ -91,8 +144,22 @@ namespace WpfDBMusicLabel.ViewModel
             {
                 case nameof(Vinyl):
                     FormatoVis = Vinyl == true ? Visibility.Visible : Visibility.Collapsed;
+                    if (!Vinyl)
+                    {
+                        Giri33 = false;
+                        Giri45 = false;
+                    }
                     break;
             }
         }
+
+        private bool CheckProd() => NewProd.Prezzo >= 0 && NewProd.QtaProdotta >= 0
+                && NewProd.CostoFornitura >= 0 && NewProd.Descrizione != "";
+
+        private bool CheckSelections() => SelectedProducer != null && SelectedAlbum != null;
+
+        private bool CheckFormat() => (Cd || Vinyl || Cassette) && (Vinyl && (Giri33 || Giri45) || (!Vinyl && !Giri33 && !Giri45));
+
+        private bool InsertCheck() => CheckFormat() && CheckSelections() && CheckProd();
     }
 }
